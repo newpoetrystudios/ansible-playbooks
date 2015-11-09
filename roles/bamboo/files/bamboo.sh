ce7069f16916f07292d88b23d9af0e025829841e
Jeff Carnegie <jeff.carnegie@gmail.com>
2015-11-09 01:33:14 -0800

    intelos build server agent playbooks

--- roles/bamboo/files/bamboo.sh
+++ roles/bamboo/files/bamboo.sh
@@ -2,4 +2,4 @@ export JAVA_HOME=/usr
 export EC2_HOME=/usr
 # export EC2_PRIVATE_KEY=/root/pk.pem
 # export EC2_CERT=/root/cert.pem
-export PATH=/opt/bamboo-elastic-agent/bin:$EC2_HOME/bin:$JAVA_HOME/bin:$M2_HOME/bin:$MAVEN_HOME/bin:$ANT_HOME/bin:$PATH
+export PATH=.node_modules/.bin:/opt/bamboo-elastic-agent/bin:$EC2_HOME/bin:$JAVA_HOME/bin:$M2_HOME/bin:$MAVEN_HOME/bin:$ANT_HOME/bin:$PATH
